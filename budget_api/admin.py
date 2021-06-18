0f03b3b4243700926c3bda5f8c9b07706c995cfc
Golem <piotr.golemiec@gmail.com>
2021-06-18 21:01:06 +0200

    Created basic models for budget_api

--- budget_api/admin.py
+++ budget_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from budget_api import models
 
 # Register your models here.
+admin.site.register(models.Budget)
+admin.site.register(models.Expense)
+admin.site.register(models.Income)
+admin.site.register(models.SharedBudget)
+admin.site.register(models.AccessType)
+admin.site.register(models.IncomeAndExpenseCategory)
--- budget_api/models.py
+++ budget_api/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Expense(models.Model):
+    title = models.CharField(max_length=255)
+    value = models.DecimalField(max_digits=9, decimal_places=2)
+    category = models.ForeignKey("IncomeAndExpenseCategory", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} {self.value}"
+
+class Income(models.Model):
+    title = models.CharField(max_length=255)
+    value = models.DecimalField(max_digits=9, decimal_places=2)
+    category = models.ForeignKey("IncomeAndExpenseCategory", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} {self.value}"
+
+class IncomeAndExpenseCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Income and expense categories"    
+
+    def __str__(self):
+        return self.name
+
+class AccessType(models.Model):
+    name = models.CharField(max_length=127)
+
+    def __str__(self):
+        return self.name
+
+class SharedBudget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    budget = models.ForeignKey("Budget", on_delete=models.CASCADE)
+    access_type = models.ForeignKey("AccessType", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.budget.name} / {self.user.username}"
+
+class Budget(models.Model):
+    name = models.CharField(max_length=255)
+    incomes = models.ManyToManyField("Income", blank=True)
+    expenses = models.ManyToManyField("Expense", blank=True)
+    owner = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    shared_with = models.ManyToManyField(User, blank=True, related_name='budgets', through='SharedBudget')
+
+    def __str__(self):
+        return f"{self.name}"
