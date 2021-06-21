ec40036214e222cc4266eaf2c63177367913b7bd
Golem <piotr.golemiec@gmail.com>
2021-06-21 02:19:02 +0200

    Shortened endpoint paths, added login_required decorators

--- FamilyBudget/urls.py
+++ FamilyBudget/urls.py
@@ -10,12 +10,12 @@ import budget_api.views as BudgetApiViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', RedirectView.as_view(url='home/')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('accounts/logout/successful/', ProjectViews.LogoutSuccessful, name='logout-successful'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='auth/logged_out.html', next_page=None), name='logout'),
     path('home/', ProjectViews.Homepage.as_view(), name='homepage'),
     path('user/create/', ProjectViews.UserCreateView.as_view(success_url="/user_created_success/"), name='user-create'),
     path('user/delete/<int:pk>/', ProjectViews.UserDeleteView.as_view(success_url="/user_deleted_success/"), name="user-delete"),
     path('user_created/', ProjectViews.UserCreated.as_view(), name='user-created'),
     path('user_deleted/', ProjectViews.UserDeleted.as_view(), name='user-deleted'),
+    # path('budget/', include('budget_api.urls')),
 ]
--- FamilyBudget/views.py
+++ FamilyBudget/views.py
@@ -1,25 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from re import template
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.views import View
 from django.urls import reverse_lazy
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout
 from django.views.generic.edit import CreateView, DeleteView
 from django.contrib.auth import get_user_model
+from django.utils.decorators import method_decorator
 
 User = get_user_model()
 
-def LogoutSuccessful(request):
-    logout(request)
-    render(request, template_name='logout_successful.html')
 
-
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class Homepage(View):
     
     def get(self, request, *args, **kwargs):
-        return render(request, 'homepage.html')
+        if request.user.is_authenticated:
+            return render(request, 'homepage.html')
+        else:
+            return redirect('login')
 
-
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class UserCreateView(CreateView):
     model = User
     fields = ['username', 'password']
@@ -28,15 +30,18 @@ class UserCreateView(CreateView):
         return reverse_lazy('user-created', kwargs={'username': self.object.username})
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class UserDeleteView(DeleteView):
     model = User
     
 # to be replaced with redirects to user list view
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class UserCreated(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'user_created.html')
 
 # to be replaced with redirects to user list view
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class UserDeleted(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'user_deleted.html')
