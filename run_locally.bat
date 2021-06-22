447ba0426f3245c77ffbcbdcb44039fff71d6bbc
Golem <piotr.golemiec@gmail.com>
2021-06-22 15:08:52 +0200

    Added batch file for running locally

---
+++ run_locally.bat
@@ -0,0 +1,6 @@
+python manage.py migrate
+py manage.py loaddata budget_api/fixtures/superuser_fixture.json
+py manage.py loaddata budget_api/fixtures/budget_api_fixture.json
+set SECRET_KEY=10932x,ciau[djbn9\82y3h109w8eyu-asu9dlk
+py manage.py runserver
+pause
