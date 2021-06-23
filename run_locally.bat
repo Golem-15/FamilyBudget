2e7da3076eceff20b18ae14f5fbac29fa4f0b5ff
Golem <piotr.golemiec@gmail.com>
2021-06-23 07:36:32 +0200

    Added local settings for run_locally.bat

--- run_locally.bat
+++ run_locally.bat
@@ -2,5 +2,5 @@ python manage.py migrate
 py manage.py loaddata budget_api/fixtures/superuser_fixture.json
 py manage.py loaddata budget_api/fixtures/budget_api_fixture.json
 set SECRET_KEY=10932x,ciau[djbn9\82y3h109w8eyu-asu9dlk
-py manage.py runserver
+py manage.py runserver --settings FamilyBudget.local_settings
 pause
