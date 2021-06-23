87a8343b39fc86cd1f464ea2efe8a09f75bb4ab3
Golem <piotr.golemiec@gmail.com>
2021-06-23 10:01:03 +0200

    Included new fixtures in run_locally.bat

--- run_locally.bat
+++ run_locally.bat
@@ -1,6 +1,6 @@
+@echo off
 set SECRET_KEY=10932x,ciau[djbn9\82y3h109w8eyu-asu9dlk
 python manage.py migrate
-py manage.py loaddata budget_api/fixtures/superuser_fixture.json
-py manage.py loaddata budget_api/fixtures/budget_api_fixture.json
-py manage.py runserver --settings FamilyBudget.local_settings
-pause
+python manage.py loaddata budget_api/fixtures/users_fixture.json
+python manage.py loaddata budget_api/fixtures/budget_api_fixture.json
+python manage.py runserver --settings FamilyBudget.local_settings
