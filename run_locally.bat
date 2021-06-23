88c4b177649a76028316fcfcad2d967f184a175a
Golem <piotr.golemiec@gmail.com>
2021-06-23 09:26:05 +0200

    Moved set key higher

--- run_locally.bat
+++ run_locally.bat
@@ -1,6 +1,6 @@
+set SECRET_KEY=10932x,ciau[djbn9\82y3h109w8eyu-asu9dlk
 python manage.py migrate
 py manage.py loaddata budget_api/fixtures/superuser_fixture.json
 py manage.py loaddata budget_api/fixtures/budget_api_fixture.json
-set SECRET_KEY=10932x,ciau[djbn9\82y3h109w8eyu-asu9dlk
 py manage.py runserver --settings FamilyBudget.local_settings
 pause
